f204aca4e095dd1d2cdf6566ebd256f1ff774b98
noavarice <alex9razinkov@gmail.com>
2017-05-26 11:05:53 +0300

    data types' and main function's declarations

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,10 @@ module Lib
     ( someFunc
     ) where
 
+type Peg = String
+type Move = (Peg, Peg)
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
