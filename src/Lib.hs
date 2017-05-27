21a4875c8cd529a5adc7e45f79847674fb86f69b
noavarice <alex9razinkov@gmail.com>
2017-05-28 01:37:01 +0300

    first working state

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,6 +6,11 @@ type Peg = String
 type Move = (Peg, Peg)
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = print $ hanoi 3 "a" "b" "c"
 
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi disksCount p1 p2 p3
+    | disksCount <= 0   = []
+    | otherwise         = (hanoi newDisksCount p1 p3 p2) ++ [(p1, p2)] ++ hanoi newDisksCount p3 p2 p1
+    where
+        newDisksCount = disksCount - 1
